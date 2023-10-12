98a05ce892ae78469198dcb968aafbe0122fdda7
alex-HAMSTER <alex2003leong@gmail.com>
2023-10-12 21:30:48 +0300

    refactoring

--- js/index.js
+++ js/index.js
@@ -3,25 +3,39 @@ const operatorMinus = document.getElementById('operatorMinus');
 const operatorUmnoz = document.getElementById('operatorUmnoz');
 const operatorDelit = document.getElementById('operatorDelit');
 const operatorPecent = document.getElementById('operatorPecent');
-
 const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
-
-
 const result = document.getElementById('result');
 
-operatorPlus.addEventListener('click', ()=> {
-    result.value = Number(num1.value) + Number(num2.value);
-})
-operatorMinus.addEventListener('click', ()=> {
-    result.value = Number(num1.value) - Number(num2.value);
-})
-operatorUmnoz.addEventListener('click', ()=> {
-    result.value = Number(num1.value) * Number(num2.value);
-})
-operatorDelit.addEventListener('click', ()=> {
-    result.value = Number(num1.value) / Number(num2.value);
-})
-operatorPecent.addEventListener('click', ()=> {
-    result.value = Number(num1.value) % Number(num2.value);
-})
+
+function onOperationButtonClick(eventOb){
+    debugger
+    var clickedElement = eventOb.currentTarget;
+    var operation = clickedElement.innerHTML;
+    makeeOperation(operation);
+}
+
+function makeeOperation(operationCode){
+    var number1 = Number(num1.value);
+    var number2 = Number(num2.value);
+    if(operationCode === '+'){
+        var res = number1 + number2;
+    }else if(operationCode === '-'){
+        var res = number1 - number2;
+    }else if(operationCode === '*'){
+        var res = number1 * number2;
+    }else if(operationCode === '/'){
+        var res = number1 / number2;
+    }else if(operationCode === '%'){
+        var res = number1 % number2;
+    }
+    result.value = res;
+}
+
+
+
+operatorPlus.addEventListener('click', onOperationButtonClick);
+operatorMinus.addEventListener('click', onOperationButtonClick);
+operatorUmnoz.addEventListener('click', onOperationButtonClick);
+operatorDelit.addEventListener('click', onOperationButtonClick);
+operatorPecent.addEventListener('click', onOperationButtonClick);
