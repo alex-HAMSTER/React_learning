d9c5cc24297af38a763496e5d38f896fb085ac6f
alex-HAMSTER <alex2003leong@gmail.com>
2023-10-20 00:20:06 +0300

    complited object

--- js/calc.js
+++ js/calc.js
@@ -1,39 +1,44 @@
-var cacl = {
+var calc = {
     operationButtons: document.getElementsByClassName('operator'),
     num1: document.getElementById('num1'),
     num2: document.getElementById('num2'),
     result: document.getElementById('result'),
 
-    makeOperation: function (operationCode) {
-        var number1 = Number(num1.value);
-        var number2 = Number(num2.value);
-        var res;
+    calcStart: function (){
+        var that = this;
+        function makeOperation (operationCode) {
+            var number1 = Number(that.num1.value);
+            var number2 = Number(that.num2.value);
+            var res;
+            
+            if (operationCode === '+'){
+                res = number1 + number2;
+            } else if(operationCode === '-'){
+                res = number1 - number2;
+            } else if(operationCode === '*'){
+                res = number1 * number2;
+            } else if(operationCode === '/'){
+                res = number1 / number2;
+            } else if(operationCode === '%'){
+                res = number1 % number2;
+            }
         
-        if (operationCode === '+'){
-            res = number1 + number2;
-        } else if(operationCode === '-'){
-            res = number1 - number2;
-        } else if(operationCode === '*'){
-            res = number1 * number2;
-        } else if(operationCode === '/'){
-            res = number1 / number2;
-        } else if(operationCode === '%'){
-            res = number1 % number2;
+            result.value = res;
         }
     
-        result.value = res;
-    },
-
-    onOperationButtonClick: function (eventObj){
-        var clickedElement = eventObj.currentTarget;
-        var operationn = clickedElement.innerHTML;
-        makeOperation(operationn);
-    }, 
-
-    for(var i = 0; i < operationButtons.length; i++){
-        var button = operationButtons[i];
-        button.addEventListener('click', onOperationButtonClick)
-    },
+        function onOperationButtonClick (eventObj) {
+            var clickedElement = eventObj.currentTarget;
+            var operationn = clickedElement.innerHTML;
+            makeOperation(operationn);
+        }
+    
+        for(var i = 0; i < that.operationButtons.length; i++){
+            var button = that.operationButtons[i];
+            button.addEventListener('click', onOperationButtonClick)
+        }
+    }
 
     
+
+
 }
--- js/index.js
+++ js/index.js
@@ -1,42 +1,4 @@
-var operationButtons = document.getElementsByClassName('operator');
-
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const result = document.getElementById('result');
-
-
-function makeOperation(operationCode) {
-    var number1 = Number(num1.value);
-    var number2 = Number(num2.value);
-    var res;
-    
-    if (operationCode === '+'){
-        res = number1 + number2;
-    } else if(operationCode === '-'){
-        res = number1 - number2;
-    } else if(operationCode === '*'){
-        res = number1 * number2;
-    } else if(operationCode === '/'){
-        res = number1 / number2;
-    } else if(operationCode === '%'){
-        res = number1 % number2;
-    }
-
-    result.value = res;
-}
-
-function onOperationButtonClick(eventObj){
-    var clickedElement = eventObj.currentTarget;
-    var operationn = clickedElement.innerHTML;
-    makeOperation(operationn);
-}
-
-
-for(var i = 0; i < operationButtons.length; i++){
-    var button = operationButtons[i];
-    button.addEventListener('click', onOperationButtonClick)
-}
-
+calc.calcStart();
 
 
 slider.arrow();
